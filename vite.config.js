ec11e20b31edd760604ce4e62f0de113d056033e
njkang4533 <njkang4533@gmail.com>
2026-08-26 13:52:37 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Baseball Salon',
+        short_name: 'BaseballSalon',
+        description: '야구 생체역학 인사이트 큐레이션 플랫폼',
+        theme_color: '#0a192f',
+        background_color: '#0a192f',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ]
 })
