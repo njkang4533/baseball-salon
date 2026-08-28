5e994fdeae8f7b003263174648acfdbff7c492fa
njkang4533 <njkang4533@gmail.com>
2026-08-28 14:04:08 +0900

    Add files via upload

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBc4Sjf5cof3zfNUjWkHs_oNaX_8XwccCY",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
